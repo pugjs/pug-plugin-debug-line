16fcd5f03b63c4856b82d9241ef183d821cb519f
Timothy Gu <timothygu99@gmail.com>
2015-11-18 17:47:04 -0800

    Rewrite using the latest Jade plugin architecture

--- index.js
+++ index.js
@@ -3,28 +3,32 @@
 var walk = require('jade-walk');
 var stringify = require('js-stringify');
 
-module.exports = function addLoc (ast) {
-  ast = JSON.parse(JSON.stringify(ast));
-  walk(ast, function (node, replace) {
-    if (node.type === 'Tag' || node.type === 'InterpolatedTag') {
-      [
-        { name: 'data-jade-file', val: node.filename, mustEscape: true },
-        { name: 'data-jade-line', val: node.line, mustEscape: false },
-        { name: 'data-jade-column', val: node.column, mustEscape: false }
-      ].forEach(function (attr) {
-        if (attr.val === undefined) return;
-        var alreadyExisted = findAttr(node.attrs, attr.name);
-        if (alreadyExisted) {
-          alreadyExisted.val = stringify(attr.val);
-          alreadyExisted.mustEscape = attr.mustEscape;
-        } else {
-          attr.val = stringify(attr.val);
-          node.attrs.push(attr);
+module.exports = function (options) {
+  return {
+    preCodeGen: function (ast) {
+      ast = JSON.parse(JSON.stringify(ast));
+      walk(ast, function (node, replace) {
+        if (node.type === 'Tag' || node.type === 'InterpolatedTag') {
+          [
+            { name: 'data-jade-file', val: node.filename, mustEscape: true },
+            { name: 'data-jade-line', val: node.line, mustEscape: false },
+            { name: 'data-jade-column', val: node.column, mustEscape: false }
+          ].forEach(function (attr) {
+            if (attr.val === undefined) return;
+            var alreadyExisted = findAttr(node.attrs, attr.name);
+            if (alreadyExisted) {
+              alreadyExisted.val = stringify(attr.val);
+              alreadyExisted.mustEscape = attr.mustEscape;
+            } else {
+              attr.val = stringify(attr.val);
+              node.attrs.push(attr);
+            }
+          });
         }
       });
+      return ast;
     }
-  });
-  return ast;
+  };
 };
 
 function findAttr (attrs, name) {
